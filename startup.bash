f5bacc08525d035a996c7ef22546633cbbb77fba
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-10-29 22:59:17 +1000

    modify startup.bash

--- startup.bash
+++ startup.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ubuntu/cab432-cloud-project
-docker compose -f docker-compose.prod.yml up --build --force-recreate -d
+docker-compose -f docker-compose.prod.yml up --build --force-recreate -d
